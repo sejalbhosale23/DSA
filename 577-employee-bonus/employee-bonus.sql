dd3ec8e4a39015e68a414679782110f0cd71a595
SEJAL BHOSALE <125639259+sejalbhosale23@users.noreply.github.com>
2026-08-27 13:26:14 +0530

    Time: 1181 ms (35.65%) | Memory: 0B (100.00%) - LeetSync

---
+++ 577-employee-bonus/employee-bonus.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+-- 
+
+select employee.name, bonus.bonus
+from employee
+left join bonus
+on employee.empId = bonus.empId
+where bonus.bonus < 1000
+or bonus.bonus Is null
