080796d0839c86d16ef7af3860045d146315ba88
SEJAL BHOSALE <125639259+sejalbhosale23@users.noreply.github.com>
2026-08-30 14:21:00 +0530

    Time: 1041 ms (70.14%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT
+SELECT 
     s.student_id,
     s.student_name,
     sub.subject_name,
@@ -9,10 +9,5 @@ CROSS JOIN Subjects sub
 LEFT JOIN Examinations e
     ON s.student_id = e.student_id
     AND sub.subject_name = e.subject_name
-GROUP BY
-    s.student_id,
-    s.student_name,
-    sub.subject_name
-ORDER BY
-    s.student_id,
-    sub.subject_name;
+GROUP BY s.student_id, s.student_name, sub.subject_name
+ORDER BY s.student_id, sub.subject_name;
